4b8cc10c6754c1f7d269d1bacc7f6f7b85be47f2
Omkar <goa11@iitbbs.ac.in>
2023-09-30 14:26:25 +0530

    feat: Init UI of frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import{ useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
 
 function App() {
+  const [sideToggle, setSideToggle] = useState<boolean>(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <NavBar onClick={() => setSideToggle(true)} userName=""/>
+          <Routes>
+            {/* <Route path="/" element={<HomeScreen />} />
+            <Route path="/page/:pageNumber" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/favourites" element={<FavouritesScreen />} />
+            <Route path="/game/:id" element={<GameScreen />} /> */}
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
